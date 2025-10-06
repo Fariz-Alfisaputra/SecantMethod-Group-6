6709d27ac262467a26b1603e66ba83cd003a43d6
zikripasya <ahmadzikri244@gmail.com>
2025-10-06 15:59:02 +0700

    Update secant.py

--- secant.py
+++ secant.py
@@ -13,3 +13,18 @@ def secant_method(function_str, x0, x1, epsilon=0.001, max_iter=100):
     curr_x = x1
     prev_f = f(prev_x)
     curr_f = f(curr_x)
+
+    while iteration <= max_iter:
+        
+        next_x = curr_x - curr_f * (curr_x - prev_x) / (curr_f - prev_f)
+        next_f = f(next_x)
+        error = abs(next_x - curr_x)
+        
+        results.append({
+            'n': iteration,
+            'x_{n-1}': prev_x,
+            'x_n': curr_x,
+            'x_{n+1}': next_x,
+            'f(x_{n+1})': next_f,
+            'e': error
+        })
